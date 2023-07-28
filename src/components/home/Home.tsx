1a8e95d3afb55c76780ded0a84299ebbd7392c9a
hin6150 <hin6150@gmail.com>
2023-07-28 14:59:41 +0900

    Feat - MainPage 기초 UI 설계(#4)

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,36 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
+import { css } from '@emotion/react';
+import { v4 as uuidv4 } from 'uuid';
+import { VideoContainer, VideoInfo } from './components';
+import theme from '../../styles/theme';
 
 const Home = () => {
-  return <div>Home</div>;
+  const repeatedVideos = Array.from({ length: 10 });
+
+  return (
+    <div
+      css={css`
+        margin-bottom: 80px;
+      `}
+    >
+      {repeatedVideos.map(() => {
+        const uniqueKey = uuidv4();
+        return (
+          <VideoContainer key={uniqueKey}>
+            <div
+              css={css`
+                width: 100%;
+                height: 240px;
+                background-color: ${theme.Gray[400]};
+              `}
+            />
+            <VideoInfo title="영상제목" author="영상작성자" view={10} uploadDate={new Date()} />
+          </VideoContainer>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Home;
