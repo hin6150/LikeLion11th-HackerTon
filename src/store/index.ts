73ccf26282dc52cca41b3da8b2feab6038a737f0
hin6150 <hin6150@gmail.com>
2023-08-06 16:48:53 +0900

    Feat - 전역 유저상태관리 추가(#9)

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import modalSlice from './modalSlice';
+import userSlice from './userSlice';
 
 const store = configureStore({
   reducer: {
     modal: modalSlice,
+    user: userSlice,
   },
 });
 
