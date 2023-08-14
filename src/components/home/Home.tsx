c75b4eb3aa67cb4eb7c0739a0bb03b93b1b6098d
hin6150 <hin6150@gmail.com>
2023-08-14 09:28:07 +0900

    Feat - GetVideo Home화면 적용(#15)

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,9 +2,20 @@
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { HomeGridContainer, VideoContainer, VideoFrame, VideoInfo } from './components';
+import { useGetVideosQuery } from '../../store/memberSlice';
 
 const Home = () => {
   const repeatedVideos = Array.from({ length: 10 });
+  const { data: videos, isLoading, isError } = useGetVideosQuery({}); // API 슬라이스의 훅을 사용합니다.
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error loading videos</div>;
+  }
+  console.log(videos);
 
   return (
     <HomeGridContainer>
